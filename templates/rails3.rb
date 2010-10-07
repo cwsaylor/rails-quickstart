ba19d8ca37a0f8333211ce935defe914f29f34a7
Chris Saylor <cwsaylor@gmail.com>
2010-10-07 16:24:06 -0700

    Removing call to old grid file.

--- templates/rails3.rb
+++ templates/rails3.rb
@@ -240,7 +240,7 @@ create_file 'app/views/layouts/application.html.erb' do <<-FILE
 <head>
   <meta charset="utf-8" />
   <title>#{site_name}</title>
-  <%= stylesheet_link_tag 'reset', 'grid', 'formtastic', 'application' %>
+  <%= stylesheet_link_tag 'reset', 'formtastic', 'application' %>
   <%= javascript_include_tag :defaults %>
   <%= csrf_meta_tag %>
 </head>
