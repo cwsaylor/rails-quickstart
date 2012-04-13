cca98b15e75f9c6ddc442b9fc464b8ada7b7f951
Chris Saylor <cwsaylor@gmail.com>
2012-04-13 09:29:34 -0700

    Moved mongo on heroku config into here and commented out for now

--- templates/mongoid.rb
+++ templates/mongoid.rb
@@ -35,3 +35,13 @@ gsub_file "test/test_helper.rb", /end/ do
 end
   RUBY
 end
+
+# For heroko
+# if File.exist? "config/mongoid.yml"
+#   gsub_file "config/mongoid.yml", "  host: <%= ENV['MONGOID_HOST'] %>", "  uri: <%= ENV['MONGOHQ_URL'] %>"
+#   gsub_file "config/mongoid.yml", "  port: <%= ENV['MONGOID_PORT'] %>", ""
+#   gsub_file "config/mongoid.yml", "  username: <%= ENV['MONGOID_USERNAME'] %>", ""
+#   gsub_file "config/mongoid.yml", "  password: <%= ENV['MONGOID_PASSWORD'] %>", ""
+#   gsub_file "config/mongoid.yml", "  database: <%= ENV['MONGOID_DATABASE'] %>", ""
+# end
+
