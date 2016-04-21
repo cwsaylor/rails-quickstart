cf964f149c1d4607d83b3b3949db894670cb7080
Chris Saylor <cwsaylor@gmail.com>
2016-04-21 12:51:44 -0700

    Update foundation

--- templates/foundation/flash_helper.rb
+++ templates/foundation/flash_helper.rb
@@ -1,20 +1,29 @@
 module FlashHelper
   DEFAULT_KEY_MATCHING = {
-    :alert     => :alert,
+    # Rails
     :notice    => :success,
-    :info      => :standard,
+    :error     => :alert,
+    # Foundation
+    :primary   => :primary,
     :secondary => :secondary,
     :success   => :success,
-    :error     => :alert
+    :warning   => :warning,
+    :alert     => :alert
   }
-  def display_flash(key_matching = {})
+  def display_flash(key_matching = {}, size = "")
     key_matching = DEFAULT_KEY_MATCHING.merge(key_matching)
 
     flash.inject "" do |message, (key, value)|
-      message += content_tag :div, data: { alert: "" }, class: "row alert-box #{key_matching[key.to_sym] || :standard}" do
-        (value + link_to("&times;".html_safe, "#", class: "close")).html_safe
+      # TODO: Set a motion ui class on closable
+      message += content_tag :div, data: { closable: "" }, class: ["callout", size, key_matching[key.to_sym] || :primary] do
+        (value + close_button).html_safe
       end
     end.html_safe
   end
-end
 
+  def close_button
+    button_tag(type: "button", class: "close-button", aria: { label: "Dismiss alert" }, data: { close: "" }) do
+      content_tag(:span, "&times;".html_safe, aria: { hidde: "true" })
+    end
+  end
+end
